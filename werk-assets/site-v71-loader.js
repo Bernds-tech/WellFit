8c539156f5ad4df242d72696c9bb124387006304
Bernds-tech <b.guggennberger@gmail.com>
2026-09-05 22:58:59 +0200

    WERK V71: korrigierten Schuldenrechner mit neuem Cache-Key ausliefern

--- werk-assets/site-v71-loader.js
+++ werk-assets/site-v71-loader.js
@@ -6,7 +6,7 @@ function loadScript(src,key){if(document.querySelector(`script[data-module="${ke
 function loadStyle(href,key){if(document.querySelector(`link[data-module="${key}"]`))return;const css=document.createElement('link');css.rel='stylesheet';css.href=href;css.dataset.module=key;document.head.appendChild(css)}
 function loadRegisterModule(){loadScript('./werk-assets/site-register-v2.js?v=20260905-v71','register-v2')}
 function loadProgramModule(){loadScript('./werk-assets/site-program-v2.js?v=20260905-v71','program-v2')}
-function loadDebtModule(){loadStyle('./werk-assets/site-debt-model.css?v=20260905-v71','debt-model-css');loadScript('./werk-assets/site-debt-model.js?v=20260905-v71','debt-model-js')}
+function loadDebtModule(){loadStyle('./werk-assets/site-debt-model.css?v=20260905-v71','debt-model-css');loadScript('./werk-assets/site-debt-model.js?v=20260905-debt-v3','debt-model-js')}
 function loadFiscalModule(){loadStyle('./werk-assets/site-fiscal-model.css?v=20260905-v71','fiscal-model-css');loadScript('./werk-assets/site-fiscal-model.js?v=20260905-v71','fiscal-model-js')}
 function loadFundingModule(){loadStyle('./werk-assets/site-funding-matrix.css?v=20260905-v71','funding-matrix-css');loadScript('./werk-assets/site-funding-matrix.js?v=20260905-v71','funding-matrix-js')}
 function loadSensitivityModule(){loadStyle('./werk-assets/site-fiscal-sensitivity.css?v=20260905-v71','fiscal-sensitivity-css');loadScript('./werk-assets/site-fiscal-sensitivity.js?v=20260905-v71','fiscal-sensitivity-js')}
